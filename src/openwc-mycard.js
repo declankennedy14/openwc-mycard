d4e2f6e7af151e22196a65f8d656531661e4ed5d
declankennedy14 <declankennedy1410@gmail.com>
2023-10-03 13:04:12 -0400

    update

--- src/openwc-mycard.js
+++ src/openwc-mycard.js
@@ -8,46 +8,90 @@ class OpenwcMycard extends LitElement {
   }
 
   static styles = css`
-    :host {
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: flex-start;
-      font-size: calc(10px + 2vmin);
-      color: #1a2b42;
-      max-width: 960px;
-      margin: 0 auto;
-      text-align: center;
-      background-color: var(--openwc-mycard-background-color);
+  .App {
+    text-align: center;
+  }
+  
+  .App-logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+  .color {
+    background-color: red;
+  }
+  body {
+    background-color: white;
+    
+  }
+  .card { 
+    background-color: grey;
+    border-style: solid;
+    border-width: 5px;
+    border-color: black;
+    border-radius: 10px;
+    width: 400px;
+    text-align: center;
+    color: white;
+  }
+  img {
+    margin: 0px;
+    width: 100%;
+    height: auto;
+  }
+  .btn-wrapper {
+    margin: 24px;
+  }
+  .btn-wrapper button {
+    font-size: 24px;
+    color: blue;
+    background-color: white;
+  }
+  .btn-wrapper button:hover {
+    color: red;
+    background-color: blue;
+    
+  }
+  .header {
+    font-size: 20px;
+    margin: 25px;
+  }
+  .card-description.hidden{
+    display: none;
+  }
+  .paragraph {
+    font-size: 20px;
+    margin: 25px;
+    color: white;
+  }
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin infinite 20s linear;
     }
-
-    main {
-      flex-grow: 1;
+  }
+  
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+  
+  .App-link {
+    color: #61dafb;
+  }
+  
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
     }
-
-    .logo {
-      margin-top: 36px;
-      animation: app-logo-spin infinite 20s linear;
+    to {
+      transform: rotate(360deg);
     }
-
-    @keyframes app-logo-spin {
-      from {
-        transform: rotate(0deg);
-      }
-      to {
-        transform: rotate(360deg);
-      }
-    }
-
-    .app-footer {
-      font-size: calc(12px + 0.5vmin);
-      align-items: center;
-    }
-
-    .app-footer a {
-      margin-left: 5px;
-    }
+  }
   `;
 
   constructor() {
@@ -58,20 +102,22 @@ class OpenwcMycard extends LitElement {
   render() {
     return html`
       <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.header}</h1>
-
-        <p>Edit <code>src/OpenwcMycard.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
+      <div class="wrapper" id="wrapper">
+      <div className="card">
+        <h1>Hello World!</h1>
+        <img src="https://elitesportsny.com/wp-content/uploads/2019/02/gettyimages-56613588-2-1362x766.jpg" alt="Rangers" />
+        <div className="paragraph">
+        <h1>New York Rangers Stanley Cup</h1>
+        <p class="card-description">On this card, there is an iconic picture of the New York Rangers Stanley Cup win in 1994. This was the last time they won the Stanley Cup, where they bested the Vancouver Canucks in a seven-game series. They won with an electric goal in overtime of game 7, which resulted in a final score of 2-1 Rangers.</p>
+        </div>
+        <div class="btn-wrapper">
+        <a href="https://hax.psu.edu">
+   <button>Details</button>
+   </a>
+   </div>
+      </div>
+      </div>
       </main>
-
       <p class="app-footer">
         🚽 Made with love by
         <a
@@ -84,5 +130,4 @@ class OpenwcMycard extends LitElement {
     `;
   }
 }
-
 customElements.define('openwc-mycard', OpenwcMycard);
